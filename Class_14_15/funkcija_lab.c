36f9318a3174f2aeb919f01c15fdd763889deae3
dzenifera16 <dzenifera12@gmail.com>
2023-12-06 11:44:16 +0200

    06_12_2023

--- Class_14_15/funkcija_lab.c
+++ Class_14_15/funkcija_lab.c
@@ -1,29 +1,34 @@
 #include<stdio.h>
 #include<math.h>
-void main(){
- double x =2.05, y, a ,S;
+double mans_cosinus(double x){
+ double a, S;
  int k = 0;
-y = cos (x/2);
- printf("y=cos(%2.f)=%2f\n",x, y);
+
+ //printf("y=cos(%2.f)=%2f\n",x, y);
  
  a = (pow(-1,k)*pow(x, 2*k))/(1.);
  S = a;
  printf("%2.f\t%8.2f\t%8.2f\n",x, a, S);
 
- while (k<3){
+ while (k<10){
    k++;
    a= a * (-1) *x*x / (4*(2*k)*(2*k-1));
    S = S + a ;
    printf("%2.f\t%8.2f\t%8.2f\n",x, a, S);
     
- }
- {
+  }
+  return S;
+}
+ void main(){
+  double x =2.05,y , yy;
+  y = cos(x/2);
+  printf("standarta funkcija cos- y=cos(%.2f/2)=%.2f\n", x, y);
+  yy = mans_cosinus(x);
+  printf("lietotāja funkcija - y=mans_cosinus(%.2f/2)=%.2f\n", x, yy);
     /* code */
  }
  
 
 
 
-
-}
 
--- Class_18/bisectiona_meth_3.c
+++ Class_18/bisectiona_meth_3.c
@@ -11,7 +11,7 @@ void main(){
     float a,x, delta_x, b ,y ,A;
     a= 0;
     b = 2*M_PI ;
-    printf("Cien. liet., ludzu, ievadi A vertibu sekojosam vienadojumam: sin(x)-A\n");
+    printf("Cien. liet., ludzu, ievadi A vertibu sekojosam vienadojumam: cos(x/2)-A\n");
     scanf("%f", &A);
     x = a;
     delta_x = 0.1;
@@ -22,7 +22,35 @@ void main(){
         x += delta_x;
     }
 
-
-
-
 }
+int fun(){
+
+float a = 0.01 , b=1.5*M_PI , x, delta_x=1.e-3/*0.001*/, funkca, funkcb, funkcx;
+int k = 0; 
+float A;
+printf("Cien. liet., ludzu, ievadi A vertibu sekojosam vienadojumam: sin(x)-A\n");
+scanf("%f", &A);
+
+funkca = sin(a)-A; funkcb = sin(b)-A;
+if (funkca*funkcb>0){
+    printf("intervala [%2f;%2f] sin(x) funkcijai", a,b);
+    printf("Saknju nav ( vai taja ir paru saknes)\n");
+    return 1;}
+printf("        sin(%7.3f)=%7.3f\t\t\t\t", a , sin(a)-A);
+
+    
+while ((b-a)>delta_x){
+        k++;
+        x = (a+b)/2;
+        if (funkca*(sin(x)-A)>0)
+        a =x;
+        else
+        b =x;
+        printf("%2d. iteracija: sin(%7.3f)=%7.3f\t",k, a , sin(a)-A);
+        printf("sin(%7.3f)=%7.3f\t",x , sin(x)-A);
+        printf("sin(%7.3f)=%7.3f\n",b , sin(b)-A);}
+
+
+printf ("Sakne atrodas pie x =%3f, jo sin(x) ir %3f\n", x , sin(x)-A);
+return 0;
+    }
