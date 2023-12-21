74363dc352e0e41e50f41f5b1a34d533003b6255
dzenifear16 <dzenifera12@gmail.com>
2023-12-21 09:43:58 +0200

    mājasdarbs

---
+++ Majasdarbi/while_factorial.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+
+
+long long calculate_factorial(long long num) {
+    long long factorial = 1;
+
+    while (num > 0) {
+        factorial *= num;
+        num--;
+    }
+
+    return factorial;
+}
+
+int main() {
+    long long num, result;
+    char data_type[20];
+
+ 
+    printf("Ievadi skaitli: ");
+    scanf("%lld", &num);
+
+    printf("Izvēlies kādu datu tipu izmantojot, vēlies attēlot skaitļa faktorialu (char-c, int-i, long long-l): ");
+    scanf("%s", data_type);
+
+ 
+    if (strcmp(data_type, "c") == 0) {
+        // strcamp salīdzina divas string vērtības, ja iznākums ir 0, tad abas vērtības ir vienādas.
+        if (num < 0 || num > 127) {
+            printf("Ar izvēlēto datu tipu šim skaitlim faktoriālu aprēķināt nav iespejams.\n");
+            return 0;
+        }
+    } else if (strcmp(data_type, "i") == 0) {
+ 
+        if (num < 0 || num > 2147483647) {
+            printf("Ar izvēlēto datu tipu šim skaitlim faktoriālu aprēķināt nav iespejams.\n");
+            return 0;
+        }
+    } else if (strcmp(data_type, "l") == 0) {
+ 
+        if (num < 0) {
+            printf("Ar izvēlēto datu tipu šim skaitlim faktoriālu aprēķināt nav iespejams.\n");
+            return 0;
+        }
+    } else {
+        printf("Nepareizi ievadīts datu tips.\n");
+        return 0;
+    }
+
+   
+    result = calculate_factorial(num);
+
+    if (result == -1) {
+        printf("Faktoriāls skaitlim, ko ievadīji nevar būt pareizi aprēķināts ar tevis izvēlēto datu tipu.\n");
+    } else {
+        printf("skaitļa faktoriāls, ko ievadīji ir  %lld\n", result);
+    }
+
+    return 0;
+}
